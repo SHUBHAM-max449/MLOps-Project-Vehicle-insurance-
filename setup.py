0c7c3cc69c8b500e5eafb8984b93d57f39b4fb54
Shubham <chitaguppeshubham007@gmail.com>
2025-06-07 19:51:42 +0200

    Project structure ready, venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="SHUBHAM-max449",
+    author_email="chitaguppeshubham007@gmail.com",
+    packages=find_packages()
+)
